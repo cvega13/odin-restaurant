3fc81fe268c72945956968b8f5f8c306ae3ab975
Carlos Vega <carlosevega03@gmail.com>
2024-08-14 21:26:49 -0400

    Add navigation functionality to header buttons
    This commit gives the buttons on the webpage's
    header the functionality to navigate to different
    tabs.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import { loadHomePage } from "./homePage";
 import { loadAboutPage } from "./aboutPage";
 import { loadMenuPage } from "./menuPage";
 
+function buttonController() {
+    function clearContent() {
+        const content = document.querySelector("#content");
+        while (content.firstChild) {
+            content.removeChild(content.lastChild);
+        }
+    }
 
+    const homeButton = document.querySelector("#homeButton");
+    homeButton.addEventListener("click", () => {
+        clearContent();
+        loadHomePage();
+    })
+    
+    const aboutButton = document.querySelector("#aboutButton");
+    aboutButton.addEventListener("click", () => {
+        clearContent();
+        loadAboutPage();
+    })
+    
+    const menuButton = document.querySelector("#menuButton");
+    menuButton.addEventListener("click", () => {
+        clearContent();
+        loadMenuPage();
+    })
+}
 
+buttonController();
 loadHomePage();
